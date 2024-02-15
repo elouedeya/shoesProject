af369564df6c368b92b11d7c59b4cf472e38e07f
eya eloued <eloued.eya98@gmail.com>
2024-02-15 16:03:32 +0100

    mobile menu responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,13 @@
+import { TbShoppingBag } from "react-icons/tb";
 import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from "react";
 
 const Routes = ["Home", "About", "Services", "Pricing", "Contact"];
 export default function Nav() {
+
+
+   const  [isMobileMenuShow , setIsMobileMenuShow]=useState(false)
   return (
     <nav className="flex flex-wrap items-center justify-between">
       {/* logo */}
@@ -10,16 +15,18 @@ export default function Nav() {
         <NikeLogo className="h-20 w-20" />
       </a>
       {/* hamburguer button */}
-      <button className="p-2 rounded-lg focus:ring-2 focus:ring-gray-200">
+      <button 
+      onClick={()=>setIsMobileMenuShow(!isMobileMenuShow)}
+      className="lg:hidden p-2 rounded-lg focus:ring-2 focus:ring-gray-200">
         <RxHamburgerMenu size={25} />
       </button>
       {/* Menu List */}
-      <div className="w-full">
-        <ul className=" rounded-lg border border-gray-100 p-4  bg-gray-50 text-lg">
+      <div className={`${isMobileMenuShow === false  && "hidden"}  w-full lg:w-auto lg:block`}>
+        <ul className="flex flex-col lg:flex-row  lg:space-x-8 rounded-lg border border-gray-100 p-4  bg-gray-50 text-lg lg:bg-transparent lg:border-none">
           {Routes.map((route, i) => {
             return (
               <li
-                className={` cursor-pointer px-3 py-2 ${i === 0 ? "bg-blue-500 text-white" : "hover:bg-gray-100"}`}
+                className={` cursor-pointer px-3 py-2 ${i === 0 ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500" : "hover:bg-gray-100"}`}
                 key={route}
               >
                 {route}
@@ -27,6 +34,12 @@ export default function Nav() {
             );
           })}
         </ul>
+      </div>
+      {/* cart button */}
+      <div className="fixed bottom-4 left-4 lg:static ">
+        <div className=" flex-center rounded-full bg-white shadow-md h-12 w-12 ">
+          <TbShoppingBag />
+        </div>
       </div>
     </nav>
   );
